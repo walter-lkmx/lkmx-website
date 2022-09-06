c9d991ed9e3e75828d88b499aaff4199a3af3a5b
Asael <asael.medina@lkmx.io>
2022-09-06 14:49:39 -0700

    Adjust styles on services page

--- src/components/header.js
+++ src/components/header.js
@@ -14,14 +14,14 @@ export default function Header(props) {
           <div className={styles.header__content}>
             <div className={styles.header__content__logo}>
               <a onClick={handleClick} className={styles.header__content__logo__menu}>
-                <img src="/icon-menu.png" alt="Logo" style={{width: '24px', height: '24px'}}/>
+                <img src="/icon-hamburguer.svg" alt="Logo" style={{width: '24px', height: '24px'}}/>
               </a>
               <Link href="/">
                 <img src="/logo.png" alt="Logo" style={{width: '121px', height: '50px', cursor: "pointer"}}/>
               </Link>
             </div>
-            <div>
-              <PagesLinks className={styles.header__content__links}/>
+            <div className={styles.header__content__links}>
+              <PagesLinks className={styles.header__content__links__pages}/>
             </div>
           </div>
         </Block>
--- src/pages/services.js
+++ src/pages/services.js
@@ -34,16 +34,18 @@ export default function ServicesPage() {
           <div className={styles.page__content__info}>
             <p>
               Nos especializamos en la construcción de<br/>
-              <strong>Soluciones de Software desde cero,</strong><br/> 
+              <strong>SOLUCIONES DE SOFTWARE DESDE CERO,</strong><br/>
               por lo que nuestros servicios cubren todas las fases del ciclo de vida del software.<br/>
+            </p>
+            <p>
               Nuestras metodologías brindan la mayor flexibilidad posible mientras mantienen bajo control los entregables, plazos de tiempo y costos.
             </p>
           </div>
           <img src="/bleed-right.png" alt="bleed-right" className={styles['page__content__bleed-right']}/>
         </div>
-        <Column mode="full">
+        <Column mode="full" className={styles['page__phases-links']}>
           <Block>
-            <div className={styles['page__phases-links']}>
+            <div>
               <PhasesSvg className={styles['page__phases-links__svg-desktop']}/>
               <PhasesMobileSvg className={styles['page__phases-links__svg-mobile']}/>
               <div className={styles['page__phases-links__arrow']}>
@@ -52,24 +54,24 @@ export default function ServicesPage() {
             </div>
           </Block>
         </Column>
-        <Column number={3} numberM={2} numberS={1} modeS="full" className={styles.page__phases}>
+        <Column numberXl={3} numberL={2} numberM={2} numberS={1} modeS="full" className={styles.page__phases}>
           <Block>
             <div className={styles.page__phases__content}>
-              <h3>Discovery</h3>
+              <h4>DISCOVERY</h4>
               <p>Nuestras soluciones comienzan mediante el estudio previo de los retos y los objetivos por cumplir. Este será el punto de inicio para el éxito del proyecto.</p>
               <img src="/arrow-right--pink.png" alt="arrow"/>
             </div>
           </Block>
           <Block>
             <div className={styles.page__phases__content}>
-              <h3>Agile<br/>Development</h3>
+              <h4>AGILE<br/>DEVELOPMENT</h4>
               <p>Con base en lo que descubrimos, creamos increíbles experiencias web y móviles que involucran manejo de datos, big data, blockchain e inteligencia artificial.</p>
               <img src="/arrow-right--pink.png" alt="arrow"/>
             </div>
           </Block>
           <Block>
             <div className={styles.page__phases__content}>
-              <h3>Agile Maintenance, Scope dev. & continuous Support</h3>
+              <h4>AGILE MAINTENANCE, SCOPE DEV. & CONTINUOUS SUPPORT</h4>
               <p>Al final del desarrollo ofrecemos servicios de mantenimiento y soporte para una estabilización y transferencia exitosa de la plataforma.</p>
               <img src="/arrow-right--pink.png" alt="arrow"/>
             </div>
