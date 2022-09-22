2f60d8b83b06e2272b992489204c2360984ed9a7
Asael <asael.medina@lkmx.io>
2022-09-22 12:03:24 -0700

    Add font Inter

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;800&display=swap" rel="stylesheet"/>
       </Head>
       <Body>
           <Main/>
