bc34a9249059708988ef3879055d1bc7d400c2ad
Walter Hurtado <walterhf@gmail.com>
2022-11-23 15:44:03 -0800

    - Added actions to switch language

--- src/components/pages-links.js
+++ src/components/pages-links.js
@@ -1,20 +1,35 @@
 import React from "react";
 import Link from "next/link";
-import sytles from "@/components/pages-links.module.scss";
+import styles from "@/components/pages-links.module.scss";
 import { ConfigContext } from "@/providers/config-provider";
 import getLang from '@/lang';
 
 export default function PagesLinks(props) {
-  const { locale } = React.useContext(ConfigContext);
+  const { locale, updateLang } = React.useContext(ConfigContext);
   const $t = getLang(locale);
+  
+  function localeToEsp() {
+    updateLang('es-ES')
+  }
+  
+  function localeToEng() {
+    updateLang('en-US')
+  }
 
   return (
-    <div className={`${sytles.links} ${props.className} ${props.whiteColor ? sytles['white-color'] : ''}`}>
-      <Link href="/services">{$t.header.module1}</Link>
-      <Link href="/methodology">{$t.header.module2}</Link>
-      <Link href="/technology">{$t.header.module3}</Link>
-      <Link href="/projects">{$t.header.module4}</Link>
-      <Link href="/about-us">{$t.header.module5}</Link>
+    <div className={`${styles.headerNavigation}`}>
+      <nav className={`${styles.links} ${props.className} ${props.whiteColor ? styles['white-color'] : ''}`}>
+        <Link href="/services">{$t.header.module1}</Link>
+        <Link href="/methodology">{$t.header.module2}</Link>
+        <Link href="/technology">{$t.header.module3}</Link>
+        <Link href="/projects">{$t.header.module4}</Link>
+        <Link href="/team">{$t.header.module5}</Link>
+      </nav>
+      <div className={`${styles.actions}`}>
+        <img src="/icons/icon-globe.svg" alt="Language" />
+        <button className={`${locale === 'es-ES' ? `${styles.active}` : ''}`} onClick={localeToEsp}>ESP</button>
+        <button className={`${locale === 'en-US' ? `${styles.active}` : ''}`} onClick={localeToEng}>ENG</button>
+      </div>
     </div>
   );
 }
