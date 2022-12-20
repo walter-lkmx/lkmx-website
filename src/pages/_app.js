e957fa33c86e9d3fa2cadf7632e75f94d9d7624a
Walter Hurtado <walterhf@gmail.com>
2022-12-20 15:45:21 -0800

    font-family fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,8 @@
 import '@lkmx/flare';
 import '@/styles/globals.scss';
-import { Inter } from '@next/font/google'
 import { Roboto_Condensed } from '@next/font/google'
 import { IBM_Plex_Sans } from '@next/font/google'
 
-const inter = Inter({ 
-    subsets: ['latin'],
-    weight: ['400', '800'],
-    variable: '--lk-font-inter'
- })
 const roboto = Roboto_Condensed({
     subsets: ['latin'],
     weight: ['400', '700'],
@@ -23,7 +17,7 @@ console.log()
 
 export default function App({ Component, pageProps }) {
     return (
-        <div className={`${inter.variable} ${roboto.variable} ${ibm.variable}`}>
+        <div className={`${roboto.variable} ${ibm.variable}`}>
             <Component {...pageProps}/>
         </div>
     );
