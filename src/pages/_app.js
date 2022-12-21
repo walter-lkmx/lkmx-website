3241019bc0390a0cf461ee41d8ef36d4efae7cb5
Walter Hurtado <walterhf@gmail.com>
2022-12-21 12:49:17 -0800

    font fix 2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import '@lkmx/flare';
 import '@/styles/globals.scss';
 import { Roboto_Condensed } from '@next/font/google'
 import { IBM_Plex_Sans } from '@next/font/google'
+import { Inter } from '@next/font/google'
 
 const roboto = Roboto_Condensed({
     subsets: ['latin'],
@@ -13,11 +14,16 @@ const ibm = IBM_Plex_Sans({
     weight: ['400', '700'],
     variable: '--lk-font-ibm'
 })
+const inter = Inter({
+    subsets: ['latin'],
+    weight: ['400', '800'],
+    variable: '--lk-font-inter'
+})
 console.log()
 
 export default function App({ Component, pageProps }) {
     return (
-        <div className={`${roboto.variable} ${ibm.variable}`}>
+        <div className={`${roboto.variable} ${ibm.variable} ${inter.variable}`}>
             <Component {...pageProps}/>
         </div>
     );
