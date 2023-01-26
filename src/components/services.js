107c2a2ea513bc078b54c6ac54dfd2ff30c938d2
Josue-Ortiz-lkmx <josue.ortiz@lkmx.io>
2023-01-26 14:23:02 -0800

    bug fixing on ProjectPage

--- src/components/services.js
+++ src/components/services.js
@@ -10,25 +10,29 @@ export default function Services() {
     const { locale } = useRouter();
     const $t = getLang(locale);
     return (
-        <Column mode="normal" modeXl="slim" modexS="full"  number="2" numberS="1" weight="left" className={styles.services__column}>
-            <Block className={styles.services__column__block}>
-                <h2>{$t.servicesComponent.title[0]} <br/>{$t.servicesComponent.title[1]} <br/>{$t.servicesComponent.title[2]}</h2>
-                <p>{$t.servicesComponent.paragraph[0]}<br/>{$t.servicesComponent.paragraph[1]}<br/>{$t.servicesComponent.paragraph[2]}</p>
-                <Link href="/services" legacyBehavior>
-                    <div
-                      className={styles.services__btn}
-                    >{$t.servicesComponent.btn}</div>
-                </Link>
-            </Block>
-            <Block className={styles.services__column__block}>
-            <div className={styles.services__column__image}>
-                    <Image
-                    priority
-                    fill
-                    src={"/lkmx-logo-inverted.gif"}
-                    alt="glitchy logo"/>
-                </div>
-            </Block>
+        <Column mode="normal" modeL="slim" modexS="full" className={styles.services__column}>
+            <Block>
+                <div className={styles.services__column__container}>
+                    <div className={styles.services__column__block}>
+                        <h2>{$t.servicesComponent.title[0]} <br/>{$t.servicesComponent.title[1]} <br/>{$t.servicesComponent.title[2]}</h2>
+                        <p>{$t.servicesComponent.paragraph[0]} <br/>{$t.servicesComponent.paragraph[1]} <br/>{$t.servicesComponent.paragraph[2]}</p>
+                        <Link href="/services" legacyBehavior>
+                            <div
+                            className={styles.services__btn}
+                            >{$t.servicesComponent.btn}</div>
+                        </Link>
+                    </div>
+                    <div className={styles.services__column__block}>
+                    <div className={styles.services__column__image}>
+                            <Image
+                            priority
+                            fill
+                            src={"/lkmx-logo-inverted.gif"}
+                            alt="glitchy logo"/>
+                        </div>
+                    </div>
+                </div>                
+            </Block>            
         </Column>
     );
 }
