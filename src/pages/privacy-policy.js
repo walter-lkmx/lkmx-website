e616a2d4c257c2dfa06f5a6226733885ca00c92a
Josue-Ortiz-lkmx <josue.ortiz@lkmx.io>
2023-02-24 16:43:29 -0800

    fixing minor bugs on PrivacyPage

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -23,57 +23,59 @@ export default function PrivacyPage() {
             <Page className={styles.privacy}>
                 <Column mode="normal" modeL="slim" className={styles.privacy__column}>
                     <Block className={styles.privacy__column__block}>
-                        <h1>{$t.privacy.title}</h1>
-                        <p>{$t.privacy.intro}</p>
-                    </Block>
-                    <Block className={styles.privacy__column__block}>
-                        <h2>{$t.privacy.section1.title}</h2>
-                        <p>{$t.privacy.section1.paragraph}</p>
-                        <ul>
-                            <li>{$t.privacy.section1.li[0]}</li>
-                        </ul>
-                    </Block>
-                    <Block className={styles.privacy__column__block}>
-                        <h2>{$t.privacy.section2.title}</h2>
-                        <p>{$t.privacy.section2.paragraph}</p>
-                        <ul>
-                            <li>{$t.privacy.section2.li[0]}</li>
-                        </ul>
-                    </Block>
-                    <Block className={styles.privacy__column__block}>
-                        <h2>{$t.privacy.section3.title}</h2>
-                        <p>{$t.privacy.section3.paragraph}</p>
-                        <p>{$t.privacy.section3.paragraph2}</p>
-                        <ul>
-                            <li>{$t.privacy.section3.li[0]}</li>
-                            <li>{$t.privacy.section3.li[1]}</li>
-                            <li>{$t.privacy.section3.li[2]}</li>
-                            <li>{$t.privacy.section3.li[3]}</li>
-                            <li>{$t.privacy.section3.li[4]}</li>
-                            <li>{$t.privacy.section3.li[5]}</li>
-                        </ul>
-                    </Block>
-                    <Block className={styles.privacy__column__block}>
-                        <h2>{$t.privacy.section4.title}</h2>
-                        <p>{$t.privacy.section4.paragraph}</p>
-                    </Block>
-                    <Block className={styles.privacy__column__block}>
-                        <h2>{$t.privacy.section5.title}</h2>
-                        <p>{$t.privacy.section5.paragraph}</p>
-                    </Block>
-                    <Block className={styles.privacy__column__block}>
-                        <h2>{$t.privacy.section6.title}</h2>
-                        <p>{$t.privacy.section6.paragraph}</p>
-                        <ul>
-                            <li>{$t.privacy.section6.li[0]}</li>
-                            <li>{$t.privacy.section6.li[1]}</li>
-                            <li>{$t.privacy.section6.li[2]}</li>
-                        </ul>
-                        <p>{$t.privacy.section6.paragraph2}</p>
-                    </Block>
-                    <Block className={styles.privacy__column__block}>
-                        <h2>{$t.privacy.section7.title}</h2>
-                        <p>{$t.privacy.section7.paragraph} <strong>{$t.privacy.section7.link}</strong></p>
+                        <div className={styles.privacy__column__block__content}>
+                            <h1>{$t.privacy.title}</h1>
+                            <p>{$t.privacy.intro}</p>
+                        </div>
+                        <div className={styles.privacy__column__block__content}>
+                            <h2>{$t.privacy.section1.title}</h2>
+                            <p>{$t.privacy.section1.paragraph}</p>
+                            <ul>
+                                <li>{$t.privacy.section1.li[0]}</li>
+                            </ul>
+                        </div>
+                        <div className={styles.privacy__column__block__content}>
+                            <h2>{$t.privacy.section2.title}</h2>
+                            <p>{$t.privacy.section2.paragraph}</p>
+                            <ul>
+                                <li>{$t.privacy.section2.li[0]}</li>
+                            </ul>
+                        </div>
+                        <div className={styles.privacy__column__block__content}>
+                            <h2>{$t.privacy.section3.title}</h2>
+                            <p>{$t.privacy.section3.paragraph}</p>
+                            <p>{$t.privacy.section3.paragraph2}</p>
+                            <ul>
+                                <li>{$t.privacy.section3.li[0]}</li>
+                                <li>{$t.privacy.section3.li[1]}</li>
+                                <li>{$t.privacy.section3.li[2]}</li>
+                                <li>{$t.privacy.section3.li[3]}</li>
+                                <li>{$t.privacy.section3.li[4]}</li>
+                                <li>{$t.privacy.section3.li[5]}</li>
+                            </ul>
+                        </div>
+                        <div className={styles.privacy__column__block__content}>
+                            <h2>{$t.privacy.section4.title}</h2>
+                            <p>{$t.privacy.section4.paragraph}</p>
+                        </div>
+                        <div className={styles.privacy__column__block__content}>
+                            <h2>{$t.privacy.section5.title}</h2>
+                            <p>{$t.privacy.section5.paragraph}</p>
+                        </div>
+                        <div className={styles.privacy__column__block__content}>
+                            <h2>{$t.privacy.section6.title}</h2>
+                            <p>{$t.privacy.section6.paragraph}</p>
+                            <ul>
+                                <li>{$t.privacy.section6.li[0]}</li>
+                                <li>{$t.privacy.section6.li[1]}</li>
+                                <li>{$t.privacy.section6.li[2]}</li>
+                            </ul>
+                            <p>{$t.privacy.section6.paragraph2}</p>
+                        </div>
+                        <div className={styles.privacy__column__block__content}>
+                            <h2>{$t.privacy.section7.title}</h2>
+                            <p>{$t.privacy.section7.paragraph} <strong>{$t.privacy.section7.link}</strong></p>
+                        </div>
                     </Block>
                 </Column>
             </Page>
