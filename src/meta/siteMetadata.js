2f7bc0c2b2c45ddebe0d403acacdc1e7b7fd54eb
JosueOrtiz <josue.ortiz@lkmx.io>
2023-03-06 10:44:29 -0800

    Updating Og image and bug fixing

--- src/meta/siteMetadata.js
+++ src/meta/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   linkedin: "https://www.linkedin.com/company/lkmx",
   twitter: "https://twitter.com/lkmx_io",
   ogType: "website",
-  ogDefaultImageEn: "https://iili.io/HKsuJe4.jpg",
-  ogDefaultImageEs: "https://iili.io/HKsuH7f.jpg",
+  ogDefaultImageEn: "/opengraph--en.jpg",
+  ogDefaultImageEs: "/opengraph--es.jpg",
 }
 
 export default siteMetadata
