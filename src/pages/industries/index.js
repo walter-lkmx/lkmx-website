a26efeae2735f403e250db086f7cc45d0c11497c
JosueOrtiz <josue.ortiz@lkmx.io>
2023-03-07 17:00:41 -0800

    IndustriesPage Upper part

--- src/pages/industries/index.js
+++ src/pages/industries/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import BaseLayout from '@/layouts/base-layout.js';
 import styles from './index.module.scss';
-import technologies from '@/service/json/technologies';
 import { Page, Block, Column } from '@lkmx/flare-react';
 import getLang from '@/lang';
 import siteMetadata from '../../meta/siteMetadata';
@@ -59,20 +58,17 @@ export default function TechnologyPage() {
           modeL="full"
           modeM="full"
         >
-          <Block className={styles.industries__hero__block}>
-            <div className={styles.industries__hero__block__left}>
-              <div>
+          <Block className={styles.industries__hero__block__left}>            
+              <div className={styles.industries__hero__block__left__content}>
                 <h2>{$t.industries.title}</h2>
                 <p>{$t.industries.hero[0]}</p>
               </div>
-            </div>
           </Block>
           <Block className={styles.industries__hero__block}>
             <div className={styles.industries__hero__block__right}>
-              <div></div>
               <div className={styles.industries__hero__block__right__waves}>
               <Image
-                  layout="fill"                
+                  fill             
                   priority={true}
                   src="/industry-hero-wave.svg" alt="waves"
                 />
@@ -88,20 +84,21 @@ export default function TechnologyPage() {
             </div>
           </Block>
         </Column>
+
         <Column className={styles.industries__intro}>
           <Block className={styles.industries__intro__block}>
             <div className={styles.lkContainer}>
-              <h2>
+            <h2>{$t.industries.catalogTitle}</h2>
+              <p>
                 {$t.industries.intro}
-              </h2>
+              </p>
             </div>
           </Block>
         </Column>
         <Column className={styles.industries__catalog}>
           <Block>
             <div className={styles.lkContainer}>
-              <div className={styles.catalogIntro}>
-                <h2>{$t.industries.catalogTitle}</h2>
+              <div className={styles.catalogIntro}>                
                 <p>
                 {$t.industries.catalogDescription}
                 </p>
