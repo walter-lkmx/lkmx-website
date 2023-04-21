effbf5c418c74695a2a6680399509effe5ae8a3b
Josue-Ortiz-lkmx <josue.ortiz@lkmx.io>
2023-04-20 17:02:10 -0700

    Header now hides and shows on response to scroll

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Column, Block } from "@lkmx/flare-react";
 import styles from "@/components/header.module.scss";
 import PagesLinks from "@/components/pages-links.js";
@@ -6,9 +6,23 @@ import Link from "next/link";
 import Image from "next/image"
 
 export default function Header(props) {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setVisible(currentScrollPos <= 0 || currentScrollPos < prevScrollPos);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos, visible]);
 
   return (
-    <header className={styles.header}>
+    <header className={styles.header}  style={{ transform: visible ? 'translateY(0)' : 'translateY(-100%)' }}>
       <Column mode="normal" modeM="full">
         <Block>
           <div className={styles.header__content}>
